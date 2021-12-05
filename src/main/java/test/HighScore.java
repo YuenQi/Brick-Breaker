d3293590bffa86faab05aebe18bdf4f53c0788ca
YuenQi <yuenqi0102@gmail.com>
2021-12-05 13:42:33 +0800

    display current score too

--- src/main/java/test/HighScore.java
+++ src/main/java/test/HighScore.java
@@ -9,11 +9,14 @@ public class HighScore extends JFrame implements ActionListener {
     JButton quitGameButton;
     JLabel label1;
     JLabel label2;
+    JLabel label3;
+    JLabel label4;
     private Wall wall;
     private String highScoreRecord;
 
     public HighScore(Wall wall){
 
+        this.wall = wall;
         highScoreRecord = wall.getHighScore();
         createQuitGameButton();
         displayText();
@@ -26,21 +29,34 @@ public class HighScore extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);//allows frame to appear in the middle of the screen, not just top left corner
         this.add(label1);
         this.add(label2);
+        this.add(label3);
+        this.add(label4);
         this.add(quitGameButton);
     }
 
     private void displayText() {
         label1 = new JLabel();
 
-        String text = "<html><h1 align = 'center'>GAME OVER</h1><br/>";
-        text = text + "<html><h2 align = 'center'>HIGH SCORE BOARD</h2>";
+        String text = "<html><h1 align = 'center'>GAME OVER</h1>";
+        text = text + "<html><h2 align = 'center'>YOUR CURRENT SCORE IS</h2>";
 
         label1.setText(text);
-        label1.setBounds(125,30,300,150);
+        label1.setBounds(110,0,300,150);
 
         label2=new JLabel();
-        label2.setText(highScoreRecord);
-        label2.setBounds(195,160,100,50);
+        label2.setText(Integer.toString(wall.getScore()));
+        label2.setBounds(220,105,100,50);
+        label2.setFont(new Font(null, Font.PLAIN, 20));
+
+        label3 = new JLabel();
+        String text2 = "<html><h2 align = 'center'>HIGH SCORE BOARD</h2>";
+        label3.setText(text2);
+        label3.setBounds(135,130,300,75);
+
+        label4=new JLabel();
+        label4.setText(highScoreRecord);
+        label4.setBounds(210,170,100,50);
+        label4.setFont(new Font(null, Font.PLAIN, 20));
     }
 
     private void createQuitGameButton() {
@@ -53,7 +69,6 @@ public class HighScore extends JFrame implements ActionListener {
         quitGameButton.setFocusable(false);//get rid of border of text
         quitGameButton.addActionListener(this);
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
